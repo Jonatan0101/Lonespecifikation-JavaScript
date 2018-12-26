9b3f0a860bb9391522266d37d51388ce5fd86b62
Jonatan <jonatan.skott@falun.ntig.se>
2018-12-26 18:46:51 +0100

    mer

--- javascript/app.js
+++ javascript/app.js
@@ -1,6 +1,8 @@
 const btnMonth = document.querySelector('#btnMonth');
 const btnHour = document.querySelector('#btnHour');
 
+const mainPage = document.querySelector('.main-container');
+const resultPage = document.querySelector('.result-container');
 // Gömda radio btns
 const radioHour = document.querySelector('#radioHour');
 const radioMonth = document.querySelector('#radioMonth')
@@ -10,6 +12,8 @@ const monthBtn = document.querySelector('#labelMonth');
 const hourBtn = document.querySelector('#labelHour');
 
 // Inputs
+const taxTable = document.querySelector('#skatteTabell')
+const customTax = document.querySelector('#customTax');
 const salaryMonth = document.querySelector('#salary')
 const salaryHour = document.querySelector('#hourSalary');
 const hours = document.querySelector('#amountHour');
@@ -24,29 +28,38 @@ const submitBtn = document.querySelector('#submit');
 // Alla inputs
 const allInputs = document.querySelectorAll('input');
 
+var requireCustom = false;
 
-
-
-monthBtn.addEventListener('click', () => {
-    clearAllInput();
+taxTable.addEventListener('click', () => {
+    
+    if(taxTable.options[taxTable.selectedIndex].value === 'custom'){
+        customTax.className = ''
+        requireCustom = true;
+    } else {
+        customTax.className = 'hidden';
+        requireCustom = false;
+    }
 });
-hourBtn.addEventListener('click', () => {
-    clearAllInput();
-});
+
+monthBtn.addEventListener("click", clearAllInput());
+hourBtn.addEventListener('click', clearAllInput());
 
 submitBtn.addEventListener('click', () => {
     let valid = firstName.value !="" && lastName.value !="" && idNum.value !="" && date.value !="";
 
+    
 
     if(radioMonth.checked){
         if(valid && salaryMonth.value != ""){
             console.log("Månad true")
+            switchPage();
         } else {
             console.log("Månad falskt")
         }
     } else if(radioHour.checked){
         if(valid && salaryHour.value != "" && hours.value != "") {
             console.log("tim true")
+            switchPage();
         } else {
             console.log("tim false")
         }
@@ -60,4 +73,10 @@ function clearAllInput(){
     });
 }
 
+function switchPage(){
+    mainPage.className = 'hidden';
+    resultPage.classList.remove('hidden');
+}
+function fillPage(a, b, c, d, e, f, g){
 
+}
