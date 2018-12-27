b3c5e53b72b21e12f41d578dbe7200c5b252b13e
Jonatan <jonatan.skott@falun.ntig.se>
2018-12-27 21:34:09 +0100

    funktioner klara

--- javascript/app.js
+++ javascript/app.js
@@ -1,3 +1,5 @@
+const body = document.querySelector('body');
+
 const btnMonth = document.querySelector('#btnMonth');
 const btnHour = document.querySelector('#btnHour');
 
@@ -12,7 +14,7 @@ const monthBtn = document.querySelector('#labelMonth');
 const hourBtn = document.querySelector('#labelHour');
 
 // Inputs
-const taxTable = document.querySelector('#skatteTabell')
+const taxTable = document.querySelector('#taxTable');
 const customTax = document.querySelector('#customTax');
 const salaryMonth = document.querySelector('#salary')
 const salaryHour = document.querySelector('#hourSalary');
@@ -30,21 +32,25 @@ const navBtn = document.querySelector('#navBtn');
 // Alla inputs
 const allInputs = document.querySelectorAll('input');
 
+// Text som ska redigeras i HTML
+const fullNameH = document.querySelector('#fullName');
+const personIdH = document.querySelector('#personId');
+const salaryTotalH = document.querySelector('#middleSection2');
+const taxRateH = document.querySelector('#taxRate');
+const taxMoneyH = document.querySelector('#middleSection3');
+const salaryOutH = document.querySelector('#salaryOut');
+const dateH = document.querySelector('#dateH');
+
 var requireCustom = false;
 
-taxTable.addEventListener('click', () => {
-    
-    if(taxTable.options[taxTable.selectedIndex].value === 'custom'){
-        customTax.className = ''
-        requireCustom = true;
-    } else {
-        customTax.className = 'hidden';
-        requireCustom = false;
-    }
-});
 
-monthBtn.addEventListener("click", clearAllInput());
-hourBtn.addEventListener('click', clearAllInput());
+monthBtn.addEventListener('click', clearAllInput);
+hourBtn.addEventListener('click', clearAllInput);
+
+// Lägger till flera event listeners för att det garanterat ska fungera oavsett vad som används
+taxTable.addEventListener('change', customTaxInput);
+taxTable.addEventListener('click', customTaxInput);
+body.addEventListener('mouseover', customTaxInput)
 
 
 navBtn.addEventListener('click', () => {
@@ -53,26 +59,30 @@ navBtn.addEventListener('click', () => {
 submitBtn.addEventListener('click', () => {
     let valid = firstName.value !="" && lastName.value !="" && idNum.value !="" && date.value !="";
 
-    
+    valid = requireCustom && (customTax.value === "" || customTax.value >= 100)? false : true;
 
     if(radioMonth.checked){
         if(valid && salaryMonth.value != ""){
             console.log("Månad true")
+            fillPage();
             switchPage(true);
+            
         } else {
             console.log("Månad falskt")
         }
     } else if(radioHour.checked){
         if(valid && salaryHour.value != "" && hours.value != "") {
-            console.log("tim true")
+            console.log("tim true");
+            fillPage();
             switchPage(true);
         } else {
             console.log("tim false")
         }
+    } else {
+        console.log("Something is very wrong :/")
     }
     
 });
-
 
 
 function switchPage(a){
@@ -85,8 +95,31 @@ function switchPage(a){
     }
     clearAllInput();
 }
-function fillPage(a, b, c, d, e, f, g){
+function fillPage(){
+    fullNameH.textContent = `${firstName.value} ${lastName.value}`;
+    personIdH.textContent = idNum.value;
+    console.log((salaryHour.value * hours.value).toFixed());
+    salaryTotalH.textContent = radioHour.checked ? 
+    `${(salaryHour.value * hours.value).toFixed()}` : salaryMonth.value;
 
+    taxRateH.textContent = requireCustom ? customTax.value.toFixed(3) : taxTable.options[taxTable.selectedIndex].value;
+
+    taxMoneyH.textContent = (taxRateH.textContent / 100 * salaryTotalH.textContent).toFixed();
+
+    salaryOutH.textContent = salaryTotalH.textContent - taxMoneyH.textContent;
+
+    dateH.textContent = date.value;
+    console.log(date.value);
+}
+
+function customTaxInput(){
+    if (taxTable.options[taxTable.selectedIndex].value === 'custom') {
+        customTax.className = ''
+        requireCustom = true;
+    } else {
+        customTax.className = 'hidden';
+        requireCustom = false;
+    }
 }
 
 function clearAllInput() {
